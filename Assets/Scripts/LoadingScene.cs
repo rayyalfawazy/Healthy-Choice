09de48208cdb583df7262542f25a6583a480f422
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2023-05-20 08:03:09 +0700

    Seperates between healthy food and unhealthy food

---
+++ Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public TMP_Text loadingText;
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingText.text = progressValue.ToString(); 
+            yield return null;
+        }
+    }
+}
