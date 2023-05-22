16273061356ac7497fa3943ef4092801dea429f0
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2023-05-22 10:43:33 +0700

    Score system on slider

--- Assets/ScoreScreen.cs
+++ Assets/ScoreScreen.cs
@@ -2,19 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreScreen : MonoBehaviour
 {
-    TMP_Text text;
+    [SerializeField] TMP_Text text;
     [SerializeField] Catcher catcher;
+    [SerializeField] Slider fillHealthySlider;
+    [SerializeField] Slider fillUnhealthySlider;
 
     private void Start()
     {
-        text = GetComponent<TMP_Text>();
+        fillHealthySlider.gameObject.SetActive(true);
     }
 
     private void Update()
     {
-        text.text = $"Score {catcher.GetScore()}";
+        if (catcher.GetScore() >= 0)
+        {
+            text.text = $"Score {catcher.GetScore()}";
+            fillHealthySlider.gameObject.SetActive(true);
+            fillHealthySlider.value = catcher.GetScore();
+            fillUnhealthySlider.gameObject.SetActive(false);
+
+            if (catcher.GetScore() == 10) 
+            {
+                // Win Statement
+                Debug.Log("Win");
+            }
+        } else if (catcher.GetScore() <= 0)
+        {
+            text.text = $"Score {catcher.GetScore()}";
+            fillUnhealthySlider.gameObject.SetActive(true);
+            fillUnhealthySlider.value = Mathf.Abs(catcher.GetScore());
+            fillHealthySlider.gameObject.SetActive(false);
+
+            if (catcher.GetScore() == -10)
+            {
+                // Lose Statement
+                Debug.Log("Lose");
+            }
+        }
     }
 }
