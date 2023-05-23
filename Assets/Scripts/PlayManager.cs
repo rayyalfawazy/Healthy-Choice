29570532eae2b750f3564599688c034b1cd583c3
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2023-05-23 11:31:52 +0700

    Add: Play manager and score stored at Play Manager

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayManager : MonoBehaviour
 {
-    [SerializeField] ScoreScreen scoreScreen;
-    [SerializeField] Catcher catcher;   
+    [SerializeField] Catcher catcher;
+    [SerializeField] int targetScore;
+    [SerializeField] UnityEvent onWin, onLose;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [System.NonSerialized] public int value;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        value = catcher.GetScore();
+        if (value == targetScore)
+        {
+            // Win Statement
+            // Debug.Log("Win");
+            onWin.Invoke();
+        }
+
+        if (value == -targetScore)
+        {
+            // Lose Statement
+            // Debug.Log("Lose");
+            onLose.Invoke();
+        }
     }
 }
--- Assets/Scripts/ScoreScreen.cs
+++ Assets/Scripts/ScoreScreen.cs
@@ -7,45 +7,30 @@ using UnityEngine.UI;
 
 public class ScoreScreen : MonoBehaviour
 {
+    [SerializeField] PlayManager playManager;
     [SerializeField] Catcher catcher;
     [SerializeField] Slider fillHealthySlider, fillUnhealthySlider;
     [SerializeField] float slideSmoothness;
-    [SerializeField] UnityEvent onWin, onLose;
 
     private void Update()
     {
-        if (catcher.GetScore() >= 0)
+
+        if (playManager.value >= 0)
         {
-            int value = catcher.GetScore();
             float currentVelocity = 0;
             float currentValue = Mathf.SmoothDamp(fillHealthySlider.value,
-                                                value, ref currentVelocity,
+                                                playManager.value, ref currentVelocity,
                                                 slideSmoothness * Time.deltaTime);
-
             fillHealthySlider.value = currentValue;
+        }
 
-            if (catcher.GetScore() == 10) 
-            {
-                // Win Statement
-                onWin.Invoke();
-            }
-        } 
-        
-        if (catcher.GetScore() <= 0)
+        if (playManager.value <= 0)
         {
-            int value = Mathf.Abs(catcher.GetScore());
             float currentVelocity = 0;
             float currentValue = Mathf.SmoothDamp(fillUnhealthySlider.value,
-                                                value, ref currentVelocity,
+                                                Mathf.Abs(playManager.value), ref currentVelocity,
                                                 slideSmoothness * Time.deltaTime);
-
             fillUnhealthySlider.value = currentValue;
-
-            if (catcher.GetScore() == -10)
-            {
-                // Lose Statement
-                onLose.Invoke();
-            }
         }
     }
 }
