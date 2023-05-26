fb91a6c004755a2924aaada265b194918cef151a
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2023-05-26 18:01:29 +0700

    Delete : pre start option and use Countdown Instead

--- Assets/PreStartScreen.cs
+++ Assets/PreStartScreen.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PreStartScreen : MonoBehaviour
 {
     [SerializeField] PlayManager playManager;
+    [SerializeField] TextMeshProUGUI preStartText;
+
     void Start()
     {
-        
+        PreStart();
+    }
+
+    public void PreStart()
+    {
+        preStartText.text = playManager.currentStage.stageName;
     }
 }
--- Assets/Scripts/GameplayScripts/Timer.cs
+++ Assets/Scripts/GameplayScripts/Timer.cs
@@ -9,10 +9,4 @@ public class Timer : MonoBehaviour
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
