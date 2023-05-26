96c887d4342b405b985e53b566d8f1a0c9a32641
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2023-05-26 14:38:31 +0700

    Pre start panel.

---
+++ Assets/PreStartScreen.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreStartScreen : MonoBehaviour
+{
+    [SerializeField] PlayManager playManager;
+    void Start()
+    {
+        
+    }
+}
