8741f8f74ad3cd3d932ef5ada11d3990c5603eff
BernardDog03 <rafifsuja03@gmail.com>
2023-05-29 15:16:09 +0700

    Add Fullscreen Toggle and Insert Asset Into UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void fullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen; 
+    }
 }
